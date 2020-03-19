a06bbfd72ac62317a335f4bb33acfda7f808e6ef
yyamakawa_pro <goooldegg@gmail.com>
2020-03-20 01:37:53 +0900

    Create | edit_MDEsetting.sh

---
+++ edit_MDEsetting.sh
@@ -0,0 +1,19 @@
+##############################################################
+#    edit_MDEsetting                                         #
+#                                                            #
+#    This shell script enables you to edit MDEsetting.sh.    #
+#    Enter following command on the terminal:                #
+#    % sh -c "$(curl -sSL https://raw.githubusercontent.com/togaritogarikun/setting/master/execute_MDE.sh)"
+##############################################################
+cd
+
+mkdir MDEsetting
+
+cd MDEsetting
+
+#git clone https://github.com/togaritogarikun/setting.git
+curl -O https://raw.githubusercontent.com/togaritogarikun/setting/master/MDEsetting.sh
+
+chmod 777 MDEsetting.sh
+
+printf '\033[33m%s\033[m\n' 'Edit MDEsetting.sh and then execute it'
