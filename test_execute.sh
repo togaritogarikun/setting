d6e393566e895fb6b30c904d78a8ce5d13477039
yyamakawa_pro <goooldegg@gmail.com>
2020-03-19 22:49:27 +0900

    remove test programs

--- test_execute.sh
+++
@@ -1,4 +0,0 @@
-type gnuplot > /dev/null 2>&1                                                                                                                         
-if [ $? -eq 0 ] ; then
-    echo gnuplot exists!
-fi
